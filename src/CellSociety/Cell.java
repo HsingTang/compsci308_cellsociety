e8e7cd1c9d6043cef574155e624528ec2ecfcf85
Carrie Hunner <clh87@duke.edu>
2019-02-01 00:01:11 -0500

    created private methods to generate 4 and 8 neighbors

--- src/CellSociety/Cell.java
+++ src/CellSociety/Cell.java
@@ -53,12 +53,51 @@ abstract class Cell {
     //generates and sets 4 neighbors
     //can be called by concrete class implementations
     private void generateFourNeighbors(){
+        int[] dRow = new int[] {0, 0, 1, -1};
+        int[] dCol = new int[] {1, -1, 0, 0};
 
+        for(int k = 0; k < dRow.length; k++){
+            int tempRow = dRow[k] + myRow;
+            int tempCol = dCol[k] + myCol;
+
+            if(inBounds(tempRow, tempCol)){
+                myNeighbors.add(myGrid[tempRow][tempCol]);
+            }
+        }
     }
 
     //generates and sets 8 neighbors
     //can be called by concrete class implementations
     private void generateEightNeighbors(){
+        int[] dRow = new int[] {-1, 0, 1};
+        int[] dCol = new int[] {-1, 0, 1};
 
+        for(int k = 0; k < dRow.length; k++){
+            for(int i = 0; i < dCol.length; i++){
+                int tempRow = dRow[k] + myRow;
+                int tempCol = dCol[i] + myCol;
+
+                //ensures not to add self
+                if(tempRow == 0 && tempCol == 0){
+                    continue;
+                }
+                else{
+                    if(inBounds(tempRow, tempCol)){
+                        myNeighbors.add(myGrid[tempRow][tempCol]);
+                    }
+                }
+            }
+        }
+    }
+
+    //checks if indices are within the grid
+    private boolean inBounds(int row, int col){
+        if(row < 0 || row > myGrid.length){
+            return false;
+        }
+        else if(col < 0 || col > myGrid[0].length){
+            return false;
+        }
+        return true;
     }
 }
