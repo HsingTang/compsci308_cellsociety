659a36fa6003897827f901654c98549bcd6410f4
Carrie Hunner <clh87@duke.edu>
2019-02-02 16:25:34 -0500

    added some comments to CellWATOR

--- src/CellSociety/CellWATOR.java
+++ src/CellSociety/CellWATOR.java
@@ -196,6 +196,11 @@ public class CellWATOR extends Cell {
     }
 
 
+    /**
+     * Finds and sets neighbors.
+     * Assumes the cell can have up to four neighbors.
+     * @param cell 2D int array of Cell objects
+     */
     @Override
     public void findNeighbors(Cell[][] cell) {
         myGrid = cell;
