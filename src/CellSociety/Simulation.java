344d57c98086102813b1e71ed1141e2ae209cb6a
Hsingchih Tang <ht114@duke.edu>
2019-02-04 15:17:22 -0500

    modified javadoc

--- src/CellSociety/Simulation.java
+++ src/CellSociety/Simulation.java
@@ -18,9 +18,9 @@ import static java.lang.Math.ceil;
 /**
  * @author Hsingchih Tang
  * Trunk of the cell society project
- * Control the simulation flow by invoking and connecting XMLParser, IntroScene, UI and Cell classes
+ * Run the simulation by invoking and connecting XMLParser, IntroScene, UI and Cell classes
  * Retrieve simulation configuration parameters from XMLParser and pass arguments into UI and Cells
- * Respond to user action of playing/resuming/stepping/switching simulation
+ * Control the simulation flow by responding to user action of playing/resuming/stepping/switching simulation
  */
 public class Simulation extends Application {
 
