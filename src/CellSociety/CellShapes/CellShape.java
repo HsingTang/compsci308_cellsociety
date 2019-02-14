f1bc87ff83ce4bcfccd52f912e13418824782620
Irene Qiao <isq@duke.edu>
2019-02-14 15:40:07 -0500

    CellShape abstract class created and subclasses started

--- src/CellSociety/CellShapes/CellShape.java
+++ src/CellSociety/CellShapes/CellShape.java
@@ -1,4 +1,21 @@
 package CellSociety.CellShapes;
 
-public class CellShape {
+import CellSociety.UI;
+import javafx.scene.shape.Polygon;
+
+abstract public class CellShape extends Polygon {
+    protected UI myUI;
+    protected int CELL_WIDTH;
+    protected int CELL_HEIGHT;
+
+    public CellShape (double[] coordinates, UI ui){
+        super(coordinates);
+        myUI = ui;
+        setCellDimensions();
+    }
+
+    protected void setCellDimensions(){
+        CELL_WIDTH = myUI.GRID_HEIGHT/myUI.GRID_ROW_NUM;
+        CELL_HEIGHT = myUI.GRID_WIDTH/myUI.GRID_COL_NUM;
+    }
 }
